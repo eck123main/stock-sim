9930ff433aa1cb0f67746e51affc77fbd5c6da5e
Rotem <rotemeck123@gmail.com>
2026-05-03 17:13:24 +1000

    feat: fix layout

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,6 @@ import { searchStock } from '../../lib/stocks'
 import PortfolioChart from '../../components/PortfolioChart'
 import StockPerformanceChart from '../../components/StockPerformanceChart'
 import PortfolioComposition from '../../components/PortfolioComposition'
-import StockDetailChart from '../../components/StockDetailChart'
 
 // Helper function to format currency with commas
 function formatCurrency(amount: number): string {
@@ -1961,9 +1960,6 @@ export default function Dashboard() {
                     🔔 Set Price Alert
                   </button>
                 </div>
-                {tradeHistory.some(t => t.ticker === selectedStock.ticker) && (
-                  <StockDetailChart ticker={selectedStock.ticker} trades={tradeHistory} darkMode={darkMode} />
-                )}
               )}
             </div>
 
